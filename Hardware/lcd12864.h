db678981b7aecd4c3fcd2e14b569ac8fe904c70d
luoqi <logeexpluoqi@outlook.com>
2020-03-14 22:47:21 +0800

    - 对LCD驱动函数进行修改， 还差一点完成

--- Hardware/lcd12864.h
+++ Hardware/lcd12864.h
@@ -2,33 +2,39 @@
  * @Author: luoqi 
  * @Date: 2019-12-24 22:38:20 
  * @Last Modified by: luoqi
- * @Last Modified time: 2020-01-08 01:01:31
+ * @Last Modified time: 2020-03-14 21:32:03
  */
 #ifndef _LCD12864_H
 #define _LCD12864_H
 
 #include "stc12c5a60s2.h"
 
-sbit LCD_BK  = P2^0; // background light
-sbit LCD_clk = P0^5; // E, serial model clock
-sbit LCD_sdi = P0^4; // R/W, serial model data
-sbit LCD_cs  = P0^3; // RS, serial model chip select, "1" is active
+sbit lcd_bk         = P2^0; // background light
+sbit lcd_clk        = P0^5; // E, serial model clock
+sbit lcd_sda_out    = P0^4; // R/W, serial model data
+sbit lcd_sda_in     = P0^4; // read sda data
+sbit lcd_cs         = P0^3; // RS, serial model chip select, "1" is active
 
-#define DISPCLR             0x01
-#define RETURNHOME          0x02
-#define ENTRYMODE           0x04
-#define DISPCTRL            0x08
-#define DISPCURSOR          0x10
-#define FUNCTIONSET         0x20
-#define CGRAMADDR           0x40
-#define DDRAMADDR           0x80
+#define LCD_WRITE_CMD       0xf8
+#define LCD_WRITE_DATA      0xfa
+#define LCD_READ_STATE      0xfc
+#define LCD_READ_DATA       0xfe   
 
-#define STANDBYMODE         0x01
-#define SCROLLCGRAMADDR     0x02
-#define REVERSE             0x04
-#define EXTENDEDFUNCTION    0x20
-#define SCROLLADDR          0x40
-#define GDRAMADDR           0x80
+#define DISP_CLR             0x01
+#define RETURN_HOME          0x02
+#define ENTRY_MODE           0x04
+#define DISP_CTRL            0x08
+#define DISP_CURSOR          0x10
+#define FUNCTION_SET         0x20
+#define CGRAM_ADDR           0x40
+#define DDRAM_ADDR           0x80
+
+#define STANDBY_MODE         0x01
+#define SCROLL_CGRAM_ADDR    0x02
+#define REVERSE              0x04
+#define EXTENDED_FUNCTION    0x20
+#define SCROLL_ADDR          0x40
+#define GDRAM_ADDR           0x80
 
 typedef enum LCD_BK_OFF_ON
 {
@@ -36,21 +42,9 @@ typedef enum LCD_BK_OFF_ON
     LCD_OFF = 1
 } LcdBk;
 
-typedef enum LCD_CMD
-{
-    LCD_WrCmd   = 0xf8,
-    LCD_WrData  = 0xfa,
-    LCD_RdState = 0xfc,
-    LCD_RdData  = 0xfe
-} LcdCmd;
-
-
 void lcd_init(void);
 void lcd_bk_off_on(LcdBk state);
-void lcd_set_dot(unsigned char x, unsigned char y);
-void lcd_put_str(unsigned char row, unsigned char col, unsigned char *puts);
-
-unsigned char lcd_chek_busy(void);
-unsigned char lcd_rw(LcdCmd cmd, unsigned char wdata);
+void lcd_write_data(unsigned char cmd, unsigned char w_data);
+unsigned char lcd_read_data(void);
 
 #endif
